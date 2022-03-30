0824ba694a931ce0b11eb74cba06a5dd037e4e27
Julia D D <98531822+juliasteckler@users.noreply.github.com>
2022-03-30 00:52:09 +0000

    Completed the lab

--- README.md
+++ README.md
@@ -8,18 +8,18 @@
 _Replace anything surrounded by the `< >` symbols._
 
 ## SUMMARY:
- < Provide a short description of your program's functionality. No more than a couple sentences. >
+ Graphs data and uses interation in functions/loops
 
 ## GRACE DAYS
-Grace days used for this assignment: < # >
+Grace days used for this assignment: <1>
 
-Grace days remaining: < # >/5
+Grace days remaining: <0/5>
 
 ## KNOWN BUGS AND INCOMPLETE PARTS:
- < What parts of the project you were not able to complete >
+ I was completely lost on Part B, I left what I think is the way to start.
 
 ## REFERENCES:
- < List any outside resources used >
+ N/A
 
 ## MISCELLANEOUS COMMENTS:
- < Anything you would like the grader to know >
+ N/A
--- main.py
+++ main.py
@@ -1,18 +1,40 @@
+import turtle
 
 def seq3np1(n):
+    count = 0
     """
         Print the 3n+1 sequence from n, terminating when it reaches 1.
         args: n (int) starting value for 3n+1 sequence
         return: None
     """
     while(n != 1):
-        print(n)
+        count = count + 1
         if(n % 2) == 0:        # n is even
             n = n // 2
         else:                 # n is odd
-            n = n * 3 + 1
-    print(n)                  # the last print is 1
+             n = n * 3 + 1
+    return count             # the last print is 1
+print(seq3np1(3))
 
 def main():
-	seq3np1(3)
+  ubr = int(input("Please enter positive integer:"))
+  start = 1
+  if ubr < start:
+    exit(main)
+  seq3np1(3)
+  for i in range(1, ubr + 1):
+    start = 1 + 1
+    seq3np1(start)
+    return start
+  print(start)
 main()
+
+print(seq3np1(3))
+
+
+  
+def draw(#interation): 
+  window = turtle.Screen()
+  will = turtle.Turtle()
+  will.speed(0)
+  count_two = 0
